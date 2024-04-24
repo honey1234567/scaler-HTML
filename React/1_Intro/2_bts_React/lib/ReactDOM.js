69c02549f04ac7e6ad63b96d41e1df37a340a299
honey1234567 <42098471+honey1234567@users.noreply.github.com>
2024-04-24 20:44:48 +0530

    Add files via upload

---
+++ React/1_Intro/2_bts_React/lib/ReactDOM.js
@@ -0,0 +1,8 @@
+let ReactDOM={};
+function render(component,root){
+    let optimizedDOM=react(component);
+    console.log("rendering to DOM");
+    root.innerHTML=optimizedDOM;
+}
+
+ReactDOM.render=render;
